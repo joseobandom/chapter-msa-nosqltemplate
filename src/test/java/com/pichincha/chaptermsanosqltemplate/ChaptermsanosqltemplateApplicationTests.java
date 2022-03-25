05edad296235bcb1dc44a164cd77af12c67a60e9
joseobando0001 <jrobando@pichincha.com>
2022-03-25 11:58:10 -0500

    Creation of project
    Dependencies

---
+++ src/test/java/com/pichincha/chaptermsanosqltemplate/ChaptermsanosqltemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pichincha.chaptermsanosqltemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaptermsanosqltemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
